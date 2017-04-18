bedbef4be6ad44510aaf7b3eaf9aa7ab8788e694
daurenast89 <dauren.ast.89@gmail.com>
2017-04-18 14:38:21 +0600

    second edit poloniex go. all data to poloniex.go

--- src/poloniex/polo/poloniex.go
+++ src/poloniex/polo/poloniex.go
@@ -7,6 +7,12 @@ import (
 	"fmt"
 	"strings"
 	"time"
+	"encoding/hex"
+	"io/ioutil"
+	"net/http"
+	"crypto/hmac"
+	"crypto/sha512"
+	"strconv"
 )
 
 const (
@@ -23,6 +29,12 @@ func New(apiKey, apiSecret string) *Poloniex {
 // poloniex represent a poloniex client
 type Poloniex struct {
 	client *client
+}
+type OrderBook struct {
+	Asks     [][]interface{} `json:"asks"`
+	Bids     [][]interface{} `json:"bids"`
+	IsFrozen int             `json:"isFrozen,string"`
+	Error    string          `json:"error"`
 }
 
 // GetTickers is used to get the ticker for all markets
@@ -112,4 +124,245 @@ func (b *Poloniex) ChartData(currencyPair string, period int, start, end time.Ti
 	}
 
 	return
+}
+type CandleStick struct {
+	Date            PoloniexDate `json:"date"`
+	High            float64      `json:"high"`
+	Low             float64      `json:"low"`
+	Open            float64      `json:"open"`
+	Close           float64      `json:"close"`
+	Volume          float64      `json:"volume"`
+	QuoteVolume     float64      `json:"quoteVolume"`
+	WeightedAverage float64      `json:"weightedAverage"`
+}
+type client struct {
+	apiKey     string
+	apiSecret  string
+	httpClient *http.Client
+	throttle   <-chan time.Time
+}
+
+var (
+	// Technically 6 req/s allowed, but we're being nice / playing it safe.
+	reqInterval = 200 * time.Millisecond
+)
+
+// NewClient return a new Poloniex HTTP client
+func NewClient(apiKey, apiSecret string) (c *client) {
+	return &client{apiKey, apiSecret, &http.Client{}, time.Tick(reqInterval)}
+}
+
+// doTimeoutRequest do a HTTP request with timeout
+func (c *client) doTimeoutRequest(timer *time.Timer, req *http.Request) (*http.Response, error) {
+	// Do the request in the background so we can check the timeout
+	type result struct {
+		resp *http.Response
+		err  error
+	}
+	done := make(chan result, 1)
+	go func() {
+		resp, err := c.httpClient.Do(req)
+		done <- result{resp, err}
+	}()
+	// Wait for the read or the timeout
+	select {
+	case r := <-done:
+		return r.resp, r.err
+	case <-timer.C:
+		return nil, errors.New("timeout on reading data from Poloniex API")
+	}
+}
+
+func (c *client) makeReq(method, resource, payload string, authNeeded bool, respCh chan<- []byte, errCh chan<- error) {
+	body := []byte{}
+	connectTimer := time.NewTimer(DEFAULT_HTTPCLIENT_TIMEOUT * time.Second)
+
+	var rawurl string
+	if strings.HasPrefix(resource, "http") {
+		rawurl = resource
+	} else {
+		rawurl = fmt.Sprintf("%s/%s", API_BASE, resource)
+	}
+
+	req, err := http.NewRequest(method, rawurl, strings.NewReader(payload))
+	if err != nil {
+		respCh <- body
+		errCh <- errors.New("You need to set API Key and API Secret to call this method")
+		return
+	}
+	if method == "POST" || method == "PUT" {
+		req.Header.Add("Content-Type", "application/json;charset=utf-8")
+	}
+	req.Header.Add("Accept", "application/json")
+
+	// Auth
+	if authNeeded {
+		if len(c.apiKey) == 0 || len(c.apiSecret) == 0 {
+			respCh <- body
+			errCh <- errors.New("You need to set API Key and API Secret to call this method")
+			return
+		}
+		nonce := time.Now().UnixNano()
+		q := req.URL.Query()
+		q.Set("apikey", c.apiKey)
+		q.Set("nonce", fmt.Sprintf("%d", nonce))
+		req.URL.RawQuery = q.Encode()
+		mac := hmac.New(sha512.New, []byte(c.apiSecret))
+		_, err = mac.Write([]byte(req.URL.String()))
+		sig := hex.EncodeToString(mac.Sum(nil))
+		req.Header.Add("apisign", sig)
+	}
+
+	resp, err := c.doTimeoutRequest(connectTimer, req)
+	if err != nil {
+		respCh <- body
+		errCh <- err
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		respCh <- body
+		errCh <- err
+		return
+	}
+	if resp.StatusCode != 200 {
+		respCh <- body
+		errCh <- errors.New(resp.Status)
+		return
+	}
+
+	respCh <- body
+	errCh <- nil
+	close(respCh)
+	close(errCh)
+}
+
+// do prepare and process HTTP request to Poloniex API
+func (c *client) do(method, resource, payload string, authNeeded bool) (response []byte, err error) {
+	respCh := make(chan []byte)
+	errCh := make(chan error)
+	<-c.throttle
+	go c.makeReq(method, resource, payload, authNeeded, respCh, errCh)
+	response = <-respCh
+	err = <-errCh
+	return
+}
+type Currency struct {
+	Name               string  `json:"name"`
+	MaxDailyWithdrawal string  `json:"maxDailyWithdrawal"`
+	TxFee              float64 `json:"txFee,string"`
+	MinConf            int     `json:"minConf"`
+	Disabled           int     `json:"disabled"`
+	Frozen             int     `json:"frozen"`
+	Delisted           int     `json:"delisted"`
+}
+
+type Currencies struct {
+	Pair map[string]Currency
+}
+type PoloniexDate struct {
+	time.Time
+}
+
+func (pd *PoloniexDate) UnmarshalJSON(data []byte) error {
+	i, err := strconv.ParseInt(string(data), 10, 64)
+	if err != nil {
+		return errors.New("Timestamp invalid (can't parse int64)")
+	}
+	pd.Time = time.Unix(i, 0)
+	return nil
+}
+
+type Tickers struct {
+	Pair map[string]Ticker
+}
+
+type Ticker struct {
+	Last          float64 `json:"last,string"`
+	LowestAsk     float64 `json:"lowestAsk,string"`
+	HighestBid    float64 `json:"highestBid,string"`
+	PercentChange float64 `json:"percentChange,string"`
+	BaseVolume    float64 `json:"baseVolume,string"`
+	QuoteVolume   float64 `json:"quoteVolume,string"`
+	IsFrozen      int     `json:"isFrozen,string"`
+	High24Hr      float64 `json:"high24hr,string"`
+	Low24Hr       float64 `json:"low24hr,string"`
+}
+type Volume map[string]float64
+
+type VolumeCollection struct {
+	TotalBTC  float64 `json:"totalBTC,string"`
+	TotalUSDT float64 `json:"totalUSDT,string"`
+	TotalXMR  float64 `json:"totalXMR,string"`
+	TotalXUSD float64 `json:"totalXUSD,string"`
+	Volumes   map[string]Volume
+}
+
+func (tc *VolumeCollection) UnmarshalJSON(b []byte) error {
+	m := make(map[string]json.RawMessage)
+	if err := json.Unmarshal(b, &m); err != nil {
+		return err
+	}
+	tc.Volumes = make(map[string]Volume)
+	for k, v := range m {
+		switch k {
+		case "totalBTC":
+			f, err := parseJSONFloatString(v)
+			if err != nil {
+				return err
+			}
+			tc.TotalBTC = f
+		case "totalUSDT":
+			f, err := parseJSONFloatString(v)
+			if err != nil {
+				return err
+			}
+			tc.TotalUSDT = f
+		case "totalXMR":
+			f, err := parseJSONFloatString(v)
+			if err != nil {
+				return err
+			}
+			tc.TotalXMR = f
+		case "totalXUSD":
+			f, err := parseJSONFloatString(v)
+			if err != nil {
+				return err
+			}
+			tc.TotalXUSD = f
+		default:
+			t := make(Volume)
+			if err := json.Unmarshal(v, &t); err != nil {
+				return err
+			}
+			tc.Volumes[k] = t
+		}
+	}
+	return nil
+}
+
+func (t *Volume) UnmarshalJSON(b []byte) error {
+	m := make(map[string]json.RawMessage)
+	if err := json.Unmarshal(b, &m); err != nil {
+		return err
+	}
+	for k, v := range m {
+		f, err := parseJSONFloatString(v)
+		if err != nil {
+			return err
+		}
+		(*t)[k] = f
+	}
+	return nil
+}
+
+func parseJSONFloatString(b json.RawMessage) (float64, error) {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return 0, err
+	}
+	return strconv.ParseFloat(s, 64)
 }
