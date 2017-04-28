3db074135960d97816a3f6af38adaf10c85a3172
dauren <dauren.ast.89@gmail.com>
2017-04-28 18:01:28 +0600

    complete percent

--- src/poloniex/main.go
+++ src/poloniex/main.go
@@ -4,210 +4,126 @@ import (
 	"fmt"
 	"poloniex/polo"
 	"strconv"
-	"os"
+	"sort"
+	"time"
 )
 
 const (
 	API_KEY    = ""
 	API_SECRET = ""
 )
+
+
+//Структура для хранения значений пар
+//AskAmount-объем спроса
+//AskPrice-цена спроса
+//BidsAmount-объем предложений
+//BidsPrice-цена предложений
 type Start struct {
-	AskAmount float64
-	AskPrice float64
-
-
+	AskAmount  float64
+	AskPrice   float64
+	BidsAmount float64
+	BidsPrice  float64
 }
-
 func main() {
-	// Poloniex client
 	poloniex := poloniex.New(API_KEY, API_SECRET)
-
-	// Get Ticker (BTC-VTC)
-	/*
-		ticker, err := poloniex.GetTicker("BTC-DRK")
-		fmt.Println(err, ticker)
-	*/
-
-	// Get Tickers
-	/*
-		tickers, err := poloniex.GetTickers()
-		if err != nil {
-			fmt.Println("Error:", err)
-		} else {
-			for key, ticker := range tickers {
-				fmt.Printf("Ticker: %s, Last: %.8f\n", key, ticker.Last)
-			}
-		}
-		tickerName := "BTC_FLO"
-		ticker, ok := tickers[tickerName]
-		if ok {
-			fmt.Printf("BTC_FLO Last: %.8f\n", ticker.Last)
-		} else {
-			fmt.Println("ticker not found - ", tickerName)
-		}
-	*/
-
-	// Get Volumes
-	/*
-		volumes, err := poloniex.GetVolumes()
-		if err != nil {
-			fmt.Println("Error:", err)
-		} else {
-			for key, volume := range volumes.Volumes {
-				fmt.Printf("Ticker: %s Value: %#v\n", key, volume["BTC"])
-			}
-		}
-	*/
-
-	// Get CandleStick chart data ( OHLCV )
-	/*
-		candles, err := client.ChartData("BTC_XMR", 300, time.Now().Add(-time.Hour), time.Now())
-		if err != nil {
-			panic(err)
-		}
-		for _, candle := range candles {
-			fmt.Printf("BTC_XMR %s\tOpened at: %f\tClosed at: %f\n", candle.Date, candle.Open, candle.Close)
-		}
-	*/
-
-	// Get markets
-	/*
-		markets, err := poloniex.GetMarkets()
-		fmt.Println(err, markets)
-	*/
-
 	// Get orders book
-	//Создание файла
-	file, err := os.Create("poloniex.txt")
+	//Создаем карту StartOrderBook для сохранения начальных значений
+	StartOrderBook := map[string]Start{}
+	orderBook, err := poloniex.GetOrderBook("all", 100)
 	if err != nil {
-		fmt.Println("Не удалось создать файл poloniex.txt")
-		return
+		fmt.Println("JSON error", err)
 	}
-	defer file.Close()
-		orderBook, err := poloniex.GetOrderBook("all",10)
+	//Проходим  по всему orderBook, по каждой паре
+	//считаем общее кол-во объема, цены спроса и предложений, считаем среднюю цену
+
+	//
+	for key, v := range orderBook {
+		AskAvg0 := 0.0
+		AskAmount0 := 0.0
+		ak:=0.0
+		for _, ask := range v.Asks {
+
+			a, _ := strconv.ParseFloat(ask[0].(string), 10) //convert interface string to float
+			AskAmount0 = AskAmount0 + ask[1].(float64)
+			AskAvg0 = AskAvg0 + a
+			ak = ak + 1
+		}
+		AskAvg0 = AskAvg0 / ak
+		BidsAvg0 := 0.0
+		BidsAmount0 := 0.0
+		bk:=0.0
+		for _, bids := range v.Bids {
+			b, _ := strconv.ParseFloat(bids[0].(string), 10)//convert interface string to float
+			BidsAmount0 = BidsAmount0 + bids[1].(float64)
+			BidsAvg0 = BidsAvg0 + b
+		}
+		BidsAvg0 = BidsAvg0 / bk
+		//формирование карты начального значения StartOrderBook
+		if _, ok := StartOrderBook[key]; !ok {
+			StartOrderBook[key] = Start{AskAmount: AskAmount0,
+				AskPrice:   AskAvg0,
+				BidsAmount: BidsAmount0,
+				BidsPrice:  BidsAvg0}
+		}
+	}
+	//получаем текущие значения для сравнения
+	CurrentOrderBook := map[string]Start{}
+	//каждую секунду получаем orderBook.
+	//Проходим  по всему orderBook, по каждой паре
+	//считаем общее кол-во объема, цены спроса и предложений, считаем среднюю цену
+	//сравниваем с начальными значениями, выводим процент изменения объема и цены
+	for {
+		orderBookCurrent, err := poloniex.GetOrderBook("all", 100)
 		if err != nil {
 			fmt.Println("JSON error", err)
 		}
-	for key,v := range orderBook {
-		fmt.Println(key, v)
-		AskAvg:= 0.0
-		AskAmount:= 0.0
-		for _, ask := range v.Asks{
+		for key, v := range orderBookCurrent {
+			AskAvg := 0.0
+			AskAmount := 0.0
+			ak:=0.0
+			for _, ask := range v.Asks {
 
-			a,_:=strconv.ParseFloat(ask[0].(string),10)//convert interface string to float
-			AskAmount = AskAmount + ask[1].(float64)
-			AskAvg = AskAvg + a
+				a, _ := strconv.ParseFloat(ask[0].(string), 10) //convert interface string to float
+				AskAmount = AskAmount + ask[1].(float64)
+				AskAvg = AskAvg + a
+			}
+			AskAvg = AskAvg / ak
+			BidsAvg := 0.0
+			BidsAmount := 0.0
+			bk:= 0.0
+			for _, bids := range v.Bids {
+				b, _ := strconv.ParseFloat(bids[0].(string), 10)
+				BidsAmount = BidsAmount + bids[1].(float64)
+				BidsAvg = BidsAvg + b
+			}
+			BidsAvg = BidsAvg / bk
+			if _, ok := StartOrderBook[key]; ok {
+				AskAmountPercent := (AskAmount/StartOrderBook[key].AskAmount - 1) * 100
+				AskPricePercent := (AskAvg/StartOrderBook[key].AskPrice - 1) * 100
+				BidAmountPercent := (BidsAmount/StartOrderBook[key].BidsAmount - 1) * 100
+				BidsPricePercent := (BidsAvg/StartOrderBook[key].BidsPrice - 1) * 100
+
+				CurrentOrderBook[key] = Start{AskAmount: AskAmountPercent,
+								AskPrice:   AskPricePercent,
+								BidsAmount: BidAmountPercent,
+								BidsPrice:  BidsPricePercent}
+			}
 		}
-		//fmt.Println("AskAmount=", AskAmount)
-		AskAmount1:= strconv.FormatFloat(AskAmount, 'f', 8, 64)
-		AskPrice:=strconv.FormatFloat(AskAvg/10, 'f', 8, 64)
-		//fmt.Println("AskPrice=", AskAvg/10)
-		BidsAvg:= 0.0
-		BidsAmount:= 0.0
-		for _, bids := range v.Bids{
-			b,_:=strconv.ParseFloat(bids[0].(string), 10)
-			BidsAmount = BidsAmount + bids[1].(float64)
-			BidsAvg = BidsAvg + b
+		//сортировка карты по ключам
+		var keys []string
+		for k := range CurrentOrderBook {
+			keys = append(keys, k)
 		}
-		//fmt.Println("BidsAmount=", BidsAmount)
-		BidsAmount1:= strconv.FormatFloat(BidsAmount, 'f', 8, 64)//conv float to str after "." - 6 symbol
-		BidsPrice:= strconv.FormatFloat(BidsAvg/10, 'f', 8, 64)
-		//fmt.Println("BidsPrice=", BidsAvg/10)
-		//открытие файла и доваление записи
-		f, err := os.OpenFile("poloniex.txt", os.O_APPEND|os.O_WRONLY, 0600)
-		if err != nil{
-			fmt.Println("Не удалось открыть файл poloniex.txt")
+		sort.Strings(keys)
+		for _, k := range keys {
+			AskAmountS:= strconv.FormatFloat(CurrentOrderBook[k].AskAmount, 'f', 2, 64)//конвертация из float64 to string
+			AskPriceS:=strconv.FormatFloat(CurrentOrderBook[k].AskPrice, 'f', 2, 64)//конвертация из float64 to string
+			BidsAmountS:=strconv.FormatFloat(CurrentOrderBook[k].BidsAmount, 'f', 2, 64)//конвертация из float64 to string
+			BidsPriceS:=strconv.FormatFloat(CurrentOrderBook[k].BidsPrice, 'f', 2, 64)//конвертация из float64 to string
+			fmt.Println(k,"\t\t","dVA:", AskAmountS, "\t\t", "dPA",
+				AskPriceS, "\t\t", "dVB", BidsAmountS, "\t\t", "dVB", BidsPriceS)
 		}
-		if _, err = f.WriteString(key + "\t" +"\t"+ "AskAmount="+ AskAmount1+"\t"+"\t"+"AskPrice="+AskPrice+"\t"+"\t"+"BidsAmount="+BidsAmount1+"\t"+"\t"+"BidsPrice="+BidsPrice+"\n"); err != nil {
-			fmt.Println("Не удалось записать строку в файл", err)
-		}
-		f.Close()
+		time.Sleep(1* time.Second) //Ждем секунду до следующего цикла
 	}
-
-
-
-
-	// Market history
-	/*
-		marketHistory, err := poloniex.GetMarketHistory("BTC-DRK", 100)
-		for _, trade := range marketHistory {
-			fmt.Println(err, trade.Timestamp.String(), trade.Quantity, trade.Price)
-		}
-	*/
-
-	// Market
-
-	// BuyLimit
-	/*
-		uuid, err := poloniex.BuyLimit("BTC-DOGE", 1000, 0.00000102)
-		fmt.Println(err, uuid)
-	*/
-
-	// BuyMarket
-	/*
-		uuid, err := poloniex.BuyLimit("BTC-DOGE", 1000)
-		fmt.Println(err, uuid)
-	*/
-
-	// Sell limit
-	/*
-		uuid, err := poloniex.SellLimit("BTC-DOGE", 1000, 0.00000115)
-		fmt.Println(err, uuid)
-	*/
-
-	// Cancel Order
-	/*
-		err := poloniex.CancelOrder("e3b4b704-2aca-4b8c-8272-50fada7de474")
-		fmt.Println(err)
-	*/
-
-	// Get open orders
-	/*
-		orders, err := poloniex.GetOpenOrders("BTC-DOGE")
-		fmt.Println(err, orders)
-	*/
-
-	// Account
-	// Get balances
-	/*
-		balances, err := poloniex.GetBalances()
-		fmt.Println(err, balances)
-	*/
-
-	// Get balance
-	/*
-		balance, err := poloniex.GetBalance("DOGE")
-		fmt.Println(err, balance)
-	*/
-
-	// Get address
-	/*
-		address, err := poloniex.GetDepositAddress("QBC")
-		fmt.Println(err, address)
-	*/
-
-	// WithDraw
-	/*
-		whitdrawUuid, err := poloniex.Withdraw("QYQeWgSnxwtTuW744z7Bs1xsgszWaFueQc", "QBC", 1.1)
-		fmt.Println(err, whitdrawUuid)
-	*/
-
-	// Get order history
-	/*
-		orderHistory, err := poloniex.GetOrderHistory("BTC-DOGE", 10)
-		fmt.Println(err, orderHistory)
-	*/
-
-	// Get getwithdrawal history
-	/*
-		withdrawalHistory, err := poloniex.GetWithdrawalHistory("all", 0)
-		fmt.Println(err, withdrawalHistory)
-	*/
-
-	// Get deposit history
-	/*
-		deposits, err := poloniex.GetDepositHistory("all", 0)
-		fmt.Println(err, deposits)
-	*/
-
 }
